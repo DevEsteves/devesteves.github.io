1ac4553ef61b9861813a07975c1b529ab91c8a63
devesteves <estevesrodri@outlook.com>
2022-07-27 20:55:50 -0300

    Updates

---
+++ precache-manifest.08d354e09a86e843cd71239fabaa58b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "496b2f5daa04b7f7bf0d",
+    "url": "/static/css/main.7997b06c.chunk.css"
+  },
+  {
+    "revision": "496b2f5daa04b7f7bf0d",
+    "url": "/static/js/main.496b2f5d.chunk.js"
+  },
+  {
+    "revision": "36b4a9ec845461dcef27",
+    "url": "/static/js/1.36b4a9ec.chunk.js"
+  },
+  {
+    "revision": "33d71b96725dde6f17cd",
+    "url": "/static/js/2.33d71b96.chunk.js"
+  },
+  {
+    "revision": "f95357712a61c3b84048",
+    "url": "/static/js/runtime~main.f9535771.js"
+  },
+  {
+    "revision": "4125d4835f5146fca19261f238d0236c",
+    "url": "/static/media/maintenance.4125d483.jpeg"
+  },
+  {
+    "revision": "021b3dcad128a974870368f47b814ed2",
+    "url": "/static/media/logo-unopar.021b3dca.png"
+  },
+  {
+    "revision": "43c3ef6c19dc8728f743fe612cc7f0a0",
+    "url": "/index.html"
+  }
+];
